234d04770e81f3f59b96b67dad8bf81768e8f23c
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-08-03 10:36:23 -1100

    basic-profile file created

--- react_with_tailwind/src/App.jsx
+++ react_with_tailwind/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <p className='bg-blue-500'>Hello world</p>
+    <div className='justify-center '>
+      <h1 className='text-blue-600  text-3*1 font-bold '>Joy Ezenwoke</h1>
+       <br></br>
+      <h2 className='justify-center text-purple-400 text-2*1 font-semi-bold'>Software Developer </h2>
+
+      <p className='justify-center text-white text-x1 font-medium'>I’m a passionate software developement student learning to building web applications using modern technologies. I enjoy solving problems through clean, efficient code. I’m committed to continuous learning and creating impactful digital solutions.</p>
     </div>
   )
 }
